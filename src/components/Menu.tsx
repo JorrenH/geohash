cbc1e6e6107d9e8832641eab04991bfb9b44dea1
Jorren <jorrenhendriks@gmail.com>
2022-11-17 23:53:09 +0100

    Add collapsable output to menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,16 +1,30 @@
-import { onMount } from "solid-js";
-import { state, setState } from "../store/state";
-import { Tooltip } from 'bootstrap';
+import { createEffect, createSignal, onMount } from "solid-js";
+import { state, setState, Corner } from "../store/state";
+import { Collapse, Tooltip } from 'bootstrap';
 import Geohash from "../geohash";
 
+import './Menu.css';
+
 export default function Menu() {
-    let zoomDOM: HTMLLabelElement | undefined;
+    let zoomDOM: HTMLLabelElement | undefined,
+        collapseDOM: HTMLDivElement | undefined;
+
+    let [collapsed, toggleCollapsed] = createSignal(window.innerWidth < 600);
+
+    // let collapse: Collapse | undefined;
 
     onMount(() => {
         new Tooltip(zoomDOM!, {
             title: 'Adjust precision according to map zoom',
             placement: 'right',
-         });
+        });
+
+        let collapse = new Collapse(collapseDOM!, { toggle: collapsed() });
+        
+        createEffect(() => {
+            if (collapsed()) collapse.hide(); else collapse.show();
+        });
+
     })
     
     function persistGeohash(e: Event & { currentTarget: HTMLInputElement }) {
@@ -20,6 +34,14 @@ export default function Menu() {
         } else {
             setState('geohash', Geohash.encode(state.lat, state.lng, state.precision));
         }
+    }
+
+    function selectCorner(e: Event & { currentTarget: HTMLInputElement }) {
+        setState('corner', e.currentTarget.value as Corner);
+    }
+
+    function copyToClipboard(e: MouseEvent) {
+
     }
 
     return (
@@ -47,9 +69,37 @@ export default function Menu() {
                 <div class="input-group mb-3">
                     <span class="input-group-text">geohash</span>
                     <input class="form-control" type="text" placeholder="Geohash" aria-label="geohash"
-                        value={state.geohash} /*onInput={modifyGeohash}*/ onInput={persistGeohash}/>
+                        value={state.geohash} onInput={persistGeohash}/>
                 </div>
                 <hr/>
+                <button onClick={e => toggleCollapsed(!collapsed())} class="btn position-absolute end-0 me-3 icon material-symbols-outlined"> {collapsed() ? 'expand_more' : 'expand_less'} </button>
+                <h5 class="card-title">Output</h5>
+                <div class="collapse" ref={collapseDOM}>
+                    <div class="bounds position-relative m-5">
+                        <input id="corner-nw" type="radio" class="btn-check" name="corner" value="northwest" onChange={selectCorner} />
+                        <label class="btn btn-outline-primary position-absolute top-0 start-0 translate-middle" for="corner-nw">northwest</label>
+
+                        <input id="corner-ne" type="radio" class="btn-check" name="corner" value="northeast" onChange={selectCorner} />
+                        <label class="btn btn-outline-primary position-absolute top-0 start-100 translate-middle" for="corner-ne">northeast</label>
+
+                        <input  id="corner-ct" type="radio" class="btn-check" name="corner" value="center" onChange={selectCorner} checked />
+                        <label class="btn btn-outline-primary position-absolute top-50 start-50 translate-middle" for="corner-ct">center</label>
+
+                        <input id="corner-sw" type="radio" class="btn-check" name="corner" value="southwest" onChange={selectCorner} />
+                        <label class="btn btn-outline-primary position-absolute top-100 start-0 translate-middle" for="corner-sw">southwest</label>
+
+                        <input id="corner-se" type="radio" class="btn-check"  name="corner" value="southeast" onChange={selectCorner} />
+                        <label class="btn btn-outline-primary position-absolute top-100 start-100 translate-middle" for="corner-se">southeast</label>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text">lat</span>
+                        <input value={state.cornerLatitude} type="number" class="form-control output" step="0.00001" placeholder="Latitude" lang="en-150" aria-label="latitude" readonly />
+                        <span class="input-group-text">lon</span>
+                        <input value={state.cornerLongitude} type="number" class="form-control output" step="0.00001" placeholder="Longitude" lang="en-150" aria-label="longitude" readonly />
+                        <button onClick={copyToClipboard} class="copy input-group-text material-symbols-outlined" data-bs-toggle="tooltip" data-bs-placement="top" title="Copied to clipboard!">assignment</button>
+                    </div>
+                </div>
+                <p class="text-sm-center mb-0"> © {new Date().getFullYear()} — Jorren Hendriks </p>
             </menu>
         </div>
     );
--- src/store/state.ts
+++ src/store/state.ts
@@ -3,15 +3,40 @@ import { createStore } from "solid-js/store";
 
 import Geohash from "../geohash";
 
+export type Corner = 'center' | 'northwest' | 'northeast' | 'southwest' | 'southeast';
+
 const [ state, setState ] = createStore({
     lat: 52.0906548,
     lng: 5.1213056,
     precision: 5,
     zoom: true,
     geohash: '',
-    get latitude() { return Number.parseFloat(this.lat.toFixed(10)) },
-    get longitude() { return Number.parseFloat(this.lng.toFixed(10)) },
+    corner: 'center' as Corner,
+    get latitude() { return truncate(this.lat) },
+    get longitude() { return truncate(this.lng) },
+    get cornerLatitude() { return truncate(getCorner(this.geohash, this.corner, 'lat')); },
+    get cornerLongitude() { return truncate(getCorner(this.geohash, this.corner, 'lng')); },
 });
+
+function truncate(nr: number, precision: number = 10) : number {
+    return Number.parseFloat(nr.toFixed(precision));
+}
+
+function getCorner(geohash: string, corner: Corner, type: 'lat' | 'lng') : number {
+    let { sw, ne } = Geohash.bounds(geohash);
+    switch (corner) {
+        case 'center':
+            return type === 'lat' ? state.lat : state.lng;
+        case 'northwest':
+            return type === 'lat' ? sw.lat : ne.lng;
+        case 'northeast':
+            return type === 'lat' ? ne.lat : ne.lng;
+        case 'southwest':
+            return type === 'lat' ? sw.lat : sw.lng;
+        case 'southeast':
+            return type === 'lat' ? ne.lat : sw.lng; 
+    }
+}
 
 createEffect(() => {
     setState('geohash', Geohash.encode(state.lat, state.lng, state.precision));
